9bd7449a815153290a5e1d35708f45d7489d8be0
takuya <55338+takuya@users.noreply.github.com>
2025-05-05 15:07:14 +0900

    add-a-assersion

--- tests/Units/Array/ArrayLastTest.php
+++ tests/Units/Array/ArrayLastTest.php
@@ -5,7 +5,8 @@ use function Takuya\Helpers\Array\array_last;
 
 class ArrayLastTest extends TestCase{
   public function test_array_last(){
-    $ret = array_last([3,2,1]);
+    $ret = array_last($a=[3,2,1]);
     $this->assertEquals(1,$ret);
+    $this->assertEquals([3,2,1],$a);
   }
 }
