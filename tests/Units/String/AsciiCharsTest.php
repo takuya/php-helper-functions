06aa5c30acc14bf2d65218fb5de6e460a271bb97
takuya <55338+takuya@users.noreply.github.com>
2025-05-05 14:44:57 +0900

    cleanup

--- tests/Units/String/AsciiCharsTest.php
+++ tests/Units/String/AsciiCharsTest.php
@@ -6,6 +6,6 @@ use function Takuya\Helpers\String\ascii_chars;
 class AsciiCharsTest extends TestCase{
   public function test_ascii_chars(){
     $this->assertSame('ABCDEFGHIJKLMNOPQRSTUVWXYZ',implode('',array_slice(ascii_chars(),0,26)));
-    $this->assertSame(strtolower('ABCDEFGHIJKLMNOPQRSTUVWXYZ'),implode('',array_slice(ascii_chars(),26)));
+    $this->assertSame('abcdefghijklmnopqrstuvwxyz',implode('',array_slice(ascii_chars(),26)));
   }
 }
