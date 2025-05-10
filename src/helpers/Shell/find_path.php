d3a97052f87ea39a12247fe27282115028cc93ab
takuya <55338+takuya@users.noreply.github.com>
2025-05-11 03:26:39 +0900

    proc_openにする

--- src/helpers/Shell/find_path.php
+++ src/helpers/Shell/find_path.php
@@ -1,15 +1,19 @@
 <?php
 
 namespace Takuya\Helpers\Shell;
-if ( !function_exists( __NAMESPACE__.'\\find_path' ) ) {
-  function find_path ( $command ): string {
-    putenv( "cmd_name={$command}" );
-    $path = trim( shell_exec( "which \"\$cmd_name\"" ) ?? '' );
-    putenv( "cmd_name" );
-    if ( empty( $path ) ) {
-      throw new \RuntimeException( "{$command} is not found in path" );
+
+use function Takuya\Helpers\String\empty_str;
+
+if( !function_exists(__NAMESPACE__.'\\find_path') ) {
+  function find_path($command):string{
+    $ret = proc_open(['which', $command], [1 => ['pipe', 'w'], 2 => ['pipe', 'w']], $io);
+    while ( proc_get_status($ret)['running'] ) {
+      usleep(100);
     }
-    
-    return $path;
+    $stdout = stream_get_contents($io[1]);
+    if( proc_get_status($ret)['exitcode'] > 0 || !empty_str(stream_get_contents($io[2])) ) {
+      throw new \RuntimeException("{$command} is not found in path");
+    }
+    return trim($stdout);
   }
 }
--- tests/Units/Shell/FindPathTest.php
+++ tests/Units/Shell/FindPathTest.php
@@ -2,10 +2,15 @@
 namespace Takuya\Tests\Units\Shell;
 use Takuya\Tests\TestCase;
 use function Takuya\Helpers\Shell\find_path;
+use function Takuya\Helpers\String\str_rand;
 
 class FindPathTest extends TestCase{
   public function test_find_path(){
     $this->assertEquals('/usr/bin/ls',find_path('ls'));
     $this->assertEquals('/usr/bin/php',find_path('php'));
+  }
+  public function test_no_exists_command_find_path(){
+    $this->expectException(\RuntimeException::class);
+    find_path('php'.str_rand());
   }
 }
