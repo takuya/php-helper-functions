13e9815eb5413bc0ec4af442ac03314cd79a4132
takuya <55338+takuya@users.noreply.github.com>
2025-05-11 05:08:24 +0900

    add trim

--- src/helpers/String/str_lines.php
+++ src/helpers/String/str_lines.php
@@ -2,10 +2,14 @@
 
 namespace Takuya\Helpers\String;
 if ( !function_exists( __NAMESPACE__.'\\str_lines' ) ) {
-  function str_lines ( string $str ): array {
+  function str_lines ( string $str,$trim=false ): array {
     $str_io = new \SplTempFileObject( strlen( $str ) );
     $str_io->fwrite( $str );
     $str_io->rewind();
-    return iterator_to_array( $str_io );
+    if($trim){
+      return array_map('trim',iterator_to_array( $str_io ) );
+    }else{
+      return iterator_to_array( $str_io );
+    }
   }
 }
--- tests/Units/String/StrLinesTest.php
+++ tests/Units/String/StrLinesTest.php
@@ -12,5 +12,9 @@ class StrLinesTest extends TestCase {
     $this->assertEquals( 1, sizeof( str_lines( "aaaa\n" ) ) );
     $this->assertEquals( 3, sizeof( str_lines( "aaaa\nbbb\nccccc\n" ) ) );
     $this->assertEquals( 3, sizeof( str_lines( "aaaa\nbbb\nccccc" ) ) );
+    //
+    $sample = chunk_split(base64_encode(random_bytes(1024)),64);
+    $this->assertStringEndsNotWith(PHP_EOL,str_lines($sample,trim:true)[0]);
+    $this->assertStringEndsWith(PHP_EOL,str_lines($sample)[0]);
   }
 }
