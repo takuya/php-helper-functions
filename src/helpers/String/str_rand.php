2e2e51f223fad01dd2a2be220b37cc97f60283af
takuya <55338+takuya@users.noreply.github.com>
2025-05-11 04:57:02 +0900

    cleanup

--- src/helpers/String/str_rand.php
+++ src/helpers/String/str_rand.php
@@ -5,7 +5,7 @@ namespace Takuya\Helpers\String;
 if ( !function_exists(__NAMESPACE__.'\\str_rand')){
   // This is for test file name only.
   // Don't use in production.
-  function str_rand($length=16):string {
-    return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
+  function str_rand($length=16,$chars='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'):string {
+    return substr(str_shuffle(str_repeat($chars, ceil($length/strlen($chars)) )),1,$length);
   }
 }
