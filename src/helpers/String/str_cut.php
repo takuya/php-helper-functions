b169c90e261e3c9d47aaae1d37a7a51d72fe5f04
takuya <55338+takuya@users.noreply.github.com>
2025-05-11 05:08:12 +0900

    update-comment

--- src/helpers/String/str_cut.php
+++ src/helpers/String/str_cut.php
@@ -2,6 +2,15 @@
 
 namespace Takuya\Helpers\String;
 if ( !function_exists( __NAMESPACE__.'\\str_cut' ) ) {
+  /**
+   * GNU utils cut command in php
+   * same to `cut -d ',' -f 1`.
+   * @param $line
+   * @param $field
+   * @param $delim
+   * @param $max
+   * @return mixed|string
+   */
   function str_cut ( $line, $field = 1, $delim = '/\s+/', $max = -1 ) {
     if ( !str_starts_with( '/', $delim ) && str_ends_with( '/', $delim ) ) {
       $delim = "/{$delim}/";
