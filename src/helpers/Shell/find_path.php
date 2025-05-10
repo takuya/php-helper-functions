5fa7fb8fdba5d42358a4ab80cacf5386cdd50c3a
takuya <55338+takuya@users.noreply.github.com>
2025-05-11 03:29:38 +0900

    file_existsに変更

--- src/helpers/Shell/find_path.php
+++ src/helpers/Shell/find_path.php
@@ -3,17 +3,15 @@
 namespace Takuya\Helpers\Shell;
 
 use function Takuya\Helpers\String\empty_str;
+use function Takuya\Helpers\String\csplit;
 
 if( !function_exists(__NAMESPACE__.'\\find_path') ) {
   function find_path($command):string{
-    $ret = proc_open(['which', $command], [1 => ['pipe', 'w'], 2 => ['pipe', 'w']], $io);
-    while ( proc_get_status($ret)['running'] ) {
-      usleep(100);
+    foreach (csplit(getenv('PATH'),":") as $path){
+      if(file_exists($full=$path.DIRECTORY_SEPARATOR.$command)) {
+        return $full;
+      }
     }
-    $stdout = stream_get_contents($io[1]);
-    if( proc_get_status($ret)['exitcode'] > 0 || !empty_str(stream_get_contents($io[2])) ) {
-      throw new \RuntimeException("{$command} is not found in path");
-    }
-    return trim($stdout);
+    throw new \RuntimeException("{$command} is not found in path");
   }
 }
